0da868789e5c4eabb13a89414cc998bb8a07e53e
aymericdd <aymeric.daurelle@datadoghq.com>
2023-09-13 10:55:48 +0200

    WIP

--- ebpf/network-tc-filter/main.go
+++ ebpf/network-tc-filter/main.go
@@ -16,11 +16,11 @@ import "C"
 
 import (
 	"flag"
-	"fmt"
-	"github.com/DataDog/chaos-controller/log"
-	"go.uber.org/zap"
-	"syscall"
 	"unsafe"
+
+	"github.com/DataDog/chaos-controller/log"
+	"github.com/aquasecurity/libbpfgo"
+	"go.uber.org/zap"
 )
 
 var (
@@ -33,19 +33,6 @@ var (
 const ValueSize = 100
 const MapName = "flags_map"
 
-type BPFMap struct {
-	name string
-	fd   C.int
-}
-
-func (b *BPFMap) Update(key, value unsafe.Pointer) error {
-	errC := C.bpf_map_update_elem(b.fd, key, value, C.ulonglong(0))
-	if errC != 0 {
-		return fmt.Errorf("failed to update map %s: %w", b.name, syscall.Errno(-errC))
-	}
-	return nil
-}
-
 func main() {
 	flag.Parse()
 	path := []byte(*nPath)
@@ -55,17 +42,20 @@ func main() {
 		logger.Fatalf("could not initialize the logger: %w", err, err)
 	}
 
-	bpfMap, err := GetMapByName("flags_map")
-	if err != nil {
-		logger.Fatalf("could not get the flags_map: %w", err, err)
+	bpfMaps := libbpfgo.GetMapsByName(MapName)
+
+	if len(bpfMaps) == 0 {
+		logger.Fatalf("%s not found", MapName)
 	}
 
-	// Update the path
+	bpfMap := bpfMaps[0]
+
+	// Update the PATH
 	if err = updateMap(uint32(0), path, ValueSize, bpfMap); err != nil {
 		logger.Fatalf("could not update the path: %w", err)
 	}
 
-	// Update the method
+	// Update the METHOD
 	if err = updateMap(uint32(1), method, ValueSize, bpfMap); err != nil {
 		logger.Fatalf("could not update the method: %w", err)
 	}
@@ -73,48 +63,11 @@ func main() {
 	logger.Infof("the %s map is updated", MapName)
 }
 
-func updateMap(key uint32, value []byte, valueSize int, bpfMap *BPFMap) error {
+func updateMap(key uint32, value []byte, valueSize int, bpfMap *libbpfgo.BPFMapLow) error {
 	valueBytes := make([]byte, valueSize)
 	copy(valueBytes[:len(value)], value)
 
-	logger.Debugf("UPDATE MAP %s key: %s, value: %s, value size: %d\n", bpfMap.name, key, value, valueSize)
+	logger.Debugf("UPDATE MAP %s key: %s, value: %s, value size: %d\n", bpfMap.Name(), key, value, valueSize)
 
 	return bpfMap.Update(unsafe.Pointer(&key), unsafe.Pointer(&valueBytes[0]))
-}
-
-func GetMapByName(name string) (*BPFMap, error) {
-	startId := C.uint(0)
-	nextId := C.uint(0)
-
-	for {
-		err := C.bpf_map_get_next_id(startId, &nextId)
-		if err != 0 {
-			return nil, fmt.Errorf("could not get the map: %w", syscall.Errno(-err))
-		}
-
-		startId = nextId + 1
-
-		fd := C.bpf_map_get_fd_by_id(nextId)
-		if fd < 0 {
-			return nil, fmt.Errorf("could not get the file descriptor of %s", name)
-		}
-
-		info := C.struct_bpf_map_info{}
-		infolen := C.uint(unsafe.Sizeof(info))
-		err = C.bpf_obj_get_info_by_fd(fd, unsafe.Pointer(&info), &infolen)
-		if err != 0 {
-			return nil, fmt.Errorf("could not get the map info: %w", syscall.Errno(-err))
-		}
-
-		mapName := C.GoString((*C.char)(unsafe.Pointer(&info.name[0])))
-		if mapName != name {
-			continue
-		}
-
-		return &BPFMap{
-			name: name,
-			fd:   fd,
-		}, nil
-	}
-	return nil, fmt.Errorf("the %s map does not exists", name)
 }

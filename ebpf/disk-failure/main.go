a5235702e4cd6abc0ac85492db4baa357964b988
aymericdd <aymeric.daurelle@datadoghq.com>
2023-11-14 16:32:24 +0100

    fix: use custom logger for libbpf
    The libbpf library used by libbpgo print log in a non json format. So we set our own logger to libbpf to log in json format.

--- ebpf/disk-failure/main.go
+++ ebpf/disk-failure/main.go
@@ -39,6 +39,23 @@ func main() {
 	logger, err = log.NewZapLogger()
 	must(err)
 
+	//ebpf.ConfigureBPFLogger(logger
+
+	bpf.SetLoggerCbs(bpf.Callbacks{
+		Log: func(level int, msg string) {
+			switch level {
+			case bpf.LibbpfDebugLevel:
+				logger.Debug(msg)
+			case bpf.LibbpfInfoLevel:
+				logger.Info(msg)
+			case bpf.LibbpfWarnLevel:
+				logger.Warn(msg)
+			default:
+				logger.Error(msg)
+			}
+		},
+	})
+
 	// Create the bpf module
 	bpfModule, err := bpf.NewModuleFromFile("/usr/local/bin/bpf-disk-failure.bpf.o")
 	must(err)
